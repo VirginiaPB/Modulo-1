c08327e38baa6696d5e92d3765f3266b226bed81
VirginiaPB <97921152+VirginiaPB@users.noreply.github.com>
2022-03-30 10:31:07 -0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+
+  return num*2^0
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+
+  return num%2
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
